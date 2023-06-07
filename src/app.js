80d522fc95074fa693142db123ea6a9facfda802
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-06-07 22:21:53 +0300

    bcrypt

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const  bodyParser = require('body-parser');
+
 const user = require('./controller/user.controller');
 const task = require('./controller/task.controller');
+const api = require('./controller/api.controller');
 
 const app = express();
 
@@ -11,6 +13,7 @@ app.use(bodyParser.json());
 
 app.use('/user', user);
 app.use('/task', task);
+app.use('/api', api);
 
 app.use((error, request, response, _next) => response.send(error.message));
 
---
+++ src/controller/api.controller.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { bildResponse } = require('../helper/bildResponse');
+const { createUser, authUser } = require('../service/api.service');
+
+const route = express.Router();
+
+route.post('/reg', async (request, response) => {
+    try {
+        const { name, surname, email, pwd } = request.body;
+        const data = await createUser(name, surname, email, pwd);
+        bildResponse(response, 200, data);
+    } catch (error) {
+        bildResponse(response, 404, error.message);
+    }
+})
+
+route.post('/auth', async (request, response) => {
+    try {
+        const { email, pwd } = request.body;
+        const data = await authUser(email, pwd);
+        bildResponse(response, 200, data);
+    } catch (error) {
+        bildResponse(response, 404, error.message);
+    }
+})
+
+module.exports = route;
