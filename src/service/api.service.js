b409a2760237b1e215c923ef6430c70a31ffa8fa
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-06-10 22:11:37 +0300

    hashedPassword

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -4,6 +4,9 @@ const bcrypt = require('bcrypt');
 const salt = 2;
 
 async function createUser(name, surname, email, pwd) {
+    const user = await getUserByEmail(email);
+    if (user.length) throw new Error('такой email уже есть ');
+
     const hashedPassword = await bcrypt.hash(pwd, salt);
     const data = await createUserDB(name, surname, email, hashedPassword);
     if (data.length == 0) throw new Error('user not created');
@@ -14,10 +17,7 @@ async function authUser(email, pwd) {
     const user = await getUserByEmail(email);
     if (!user.length) throw new Error('email not found');
 
-    console.log(user);
-    console.log(email, pwd);
     const bool = await bcrypt.compare(pwd, user[0].pwd);
-    console.log(bool);
     if (!bool) throw new Error('пароли не совпадают');
     return user;
 }
